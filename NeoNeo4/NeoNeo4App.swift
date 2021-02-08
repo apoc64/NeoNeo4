b7697a73b36cb6f59e1731463cbe7015c5383831
Steve Schwedt <sschwedt@gmail.com>
2021-02-07 19:37:46 -0700

    Initial Commit

---
+++ NeoNeo4/NeoNeo4App.swift
@@ -0,0 +1,17 @@
+//
+//  NeoNeo4App.swift
+//  NeoNeo4
+//
+//  Created by Steve Schwedt on 2/7/21.
+//
+
+import SwiftUI
+
+@main
+struct NeoNeo4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
